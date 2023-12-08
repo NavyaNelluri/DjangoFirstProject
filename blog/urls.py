e548675776b2232a699eda1ada90664d07a4076c
NavyaNelluri <123142678+NavyaNelluri@users.noreply.github.com>
2023-12-08 11:55:42 -0600

    Delete blog directory

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-# blog/urls.py
-from django.urls import path
-from .views import post_list, post_detail
-
-urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('post/<int:pk>/', post_detail, name='post_detail'),
-]
